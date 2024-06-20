25c2a387c5a341d0ffafbcf899df64ac76057abc
Be1D1 <hitDim246789@gmail.com>
2024-06-21 00:28:24 +0300

    Добавлено компоненты и окно авторизации

--- KPTC_Drive/README.md
+++ KPTC_Drive/README.md
@@ -1,4 +1,4 @@
-# KPTC_Drive
+# titi
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- KPTC_Drive/src/App.vue
+++ KPTC_Drive/src/App.vue
@@ -1,85 +1,142 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="auth-container" >
+    <h2>Авторизация твоей личности</h2>
+      <div class="user">
+        <label for="username">Лицо попроще сделай! Логин давай:</label>
+        <input type="text" id="username" v-model="credentials.username" required>
+      </div>
+      <div class="pass">
+        <label for="password">Что там скрываешь? Показывай! :</label>
+        <input type="password" id="password" v-model="credentials.password" required>
+      </div>
+      <button type="button" class="in" @click="login">Войти</button>
+      <button type="button" class="backgo" @click="back">Назад</button>
+      <img src="\public\3.png" class="image">
+      <img src="\public\2.png" class="image_av">
+      <img src="\public\1.png" class="st_image">
+      <p v-if="errorMessage">{{ errorMessage }}</p>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      credentials: {
+        username: '',
+        password: ''
+      },
+      errorMessage: ''
+    };
+  },
+  methods: {
+    back() {
+      window.history.back();
+    },
+    login() {  
+      // для бэка
+    }
+  }
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+h2 {
+  font-size: 20px;
 }
 
-.logo {
+.auth-container {
+  font-family: 'Roboto', sans-serif;
+  background-color: #ffffff;
+  padding: 20px;
+  border: #ffffff;
+  border-radius: 5px;
+  width: 300px;
+  margin: auto;
+  font-size: 13px;
+}
+
+.user {
+  margin-bottom: 20px;
+}
+.pass {
+  margin-bottom: 20px;
+}
+
+input[type="text"],
+input[type="password"] {
+  width: 250px;
+  padding: 8px;
+  margin-top: 6px;
+  border: 1px solid #000000;
+  border-radius: 4px;
+  font-size: 16px;
+}
+
+.in {
+  width: 268px;
+  margin-right: 30px;
+  margin-bottom: -20px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #3f6fff;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  
+}
+
+.in:hover {
+  background-color: #009da8;
+}
+.backgo{
+  position: absolute;
+  width: 75px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #3f6fff;
+  font-size: 16px;
+  cursor: pointer;
+  color: white;
+  transition: background-color 0.3s ease;
+}
+.backgo:hover {
+  background-color: #009da8;
+}
+
+.image {
+  position: absolute;
+  width: 150px; 
+  height: 150px; 
+  margin: -200px;
   display: block;
-  margin: 0 auto 2rem;
+  margin-top: -126px
+  
+}
+.image_av{
+  position: absolute;
+  width: 150px; 
+  height: 150px; 
+  margin: 90px ;
+  display: block;
+  margin-top: 30px;
+  
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.st_image{
+  position: absolute;
+  width: 150px; 
+  height: 150px; 
+  margin: 100px;
+  display: block;
+  margin-left: -100px;
+  background-color: #ffffff;
+  transform: scaleY(-1);
+  margin-top: 30px;
 }
 </style>
+
