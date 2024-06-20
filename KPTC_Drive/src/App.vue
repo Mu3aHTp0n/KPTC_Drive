5ccaee17b90b09b3182f232ba4aa4bbde6fffe73
Dokchansky <bironincres@gmail.com>
2024-06-21 01:17:24 +0300

    Добавление окна настройки пользователя

--- KPTC_Drive/src/App.vue
+++ KPTC_Drive/src/App.vue
@@ -5,81 +5,111 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <button @click="goHome()" class="backbutton">⭠</button> 
+  <div class = 'container'>
+      <h1>Настройка пользователя</h1>
+      <input type="text" placeholder="Введите фамилию" v-model="lastName">
+      <input type="text" placeholder="Введите имя" v-model="firstName">
+      <input type="text" placeholder="Введите отчество" v-model="middleName">
+      <input type="password" placeholder="Введите старый пароль" v-model="useroldPassword">
+      <input type="password" placeholder="Введите новый пароль" v-model="userPassword">
+      <input type="password" placeholder="Подтвердите пароль" v-model="userconfirmPassword">
+      <button @click="saveData()" class="saveButton">Сохранить</button>
+      
     </div>
   </header>
-
-  <RouterView />
+  
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+        lastName: '',
+        firstName: '',
+        middleName: '',
+        useroldPassword: '',
+        userPassword: '',
+        userconfirmPassword: '',
+    };
+    },
+    methods:{
+      saveData(){ // метод кнопкни Сохранить
+        
+      },
+      goHome(){
+        window.open("https://www.youtube.com/watch?v=i_gCg5Tp-rs"); // Тут крч нужен метод открытия главного окна, а я не шарю его название, сами добавите
+      }
+    }
+}
+
+
+
+  
+  
+</script>
+
 <style scoped>
+.user-form {
+  max-width: 300px;
+  margin: auto;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
+
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+ .container{
+  font-family: 'Roboto', sans-serif;
+  background-color: #f9f9f9;
+  padding: 20px;
+  border: #ffffff;
+  border-radius: 5px;
+  width: 300px;
+  margin: auto;
+  font-size: 13px;
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
+ }
+ .container input{
+  width: 100%;
+  background: transparent;
+  padding: 8px;
+  margin: 5px 0;
   display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
   border: 0;
+  outline: none;
+  border-bottom: 2px solid hsla(160, 100%, 37%, 1);
+  box-sizing: border-box;
+ }
+ .backbutton{
+  background-color: hsla(160, 100%, 37%, 1);
+  color: white;
+  border: 2px solid hsla(160, 100%, 37%, 1);
+  text-align: center;
+  font-size: 16px;
+  border-radius: 5px;
+  position: fixed;
+}
+.saveButton{
+  background-color: hsla(160, 100%, 37%, 1);
+  color: white;
+  padding: 20px;
+  border: 2px solid hsla(160, 100%, 37%, 1);
+  text-align: center;
+  font-size: 16px;
+  margin: auto; 
+  border-radius: 5px;
+  width: 260px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.container h1{
+  color: hsla(160, 100%, 37%, 1);
+  text-align: center;
 }
+
 </style>
+
+
+
